58dce3189e5e6ef7ff1304b0a58c1cd0b84f21d4
alpsilva <alps2@cin.ufpe.br>
2020-09-12 22:55:50 -0300

    início do remake do site

--- Pedro_Lyrics.js
+++ Pedro_Lyrics.js
@@ -1,30 +1,35 @@
+$(document).ready(function () {
+    $('#botao').click(function (e) {
+        e.preventDefault();
+        pedroLyrics();
+    });
 
+    function pedroLyrics() {
+        var musica = $('#preText').val();
+        var n_musica = ""; //musica processada
+        var array_linhas = musica.split(/\r?\n/); // separa musica em linhas (versos)
+        var linhas = array_linhas.length;
+        for (var i = 0; i < linhas; i++) {
+            var array_palavras = array_linhas[i].split(' ') //separa linhas em palavras
+            var palavras = array_palavras.length;
+            console.log(array_palavras);
+            console.log(palavras);
+            var aux = Math.floor(Math.random() * palavras) + 1 //seleciona uma palavra aleatoria p/ substituir
+            if (aux == 1) {
+                aux++ //Para garantir que a primeira posição do array (vazia) nunca seja escolhida
+            }
+            var n_linha = ""; //nova linha
+            console.log(aux);
+            for (var j = 0; j < palavras; j++) {
+                if (j == aux - 1) {
+                    array_palavras[j] = "PEDRO" //substitui a palavra por PEDRO
+                }
+                n_linha = n_linha.concat(" ", array_palavras[j]);
 
-var musica = message.substring(6);
-var n_musica = ""; //musica processada
-var array_linhas = musica.split(/\r?\n/); // separa musica em linhas (versos)
-var linhas = array_linhas.length;
-for (var i = 0; i < linhas; i++) {
-    var array_palavras = array_linhas[i].split(' ') //separa linhas em palavras
-    var palavras = array_palavras.length;
-    console.log(array_palavras);
-    console.log(palavras);
-    var aux = Math.floor(Math.random() * palavras) + 1 //seleciona uma palavra aleatoria p/ substituir
-    if (aux == 1) {
-        aux++ //Para garantir que a primeira posição do array (vazia) nunca seja escolhida
-    }
-    var n_linha = ""; //nova linha
-    console.log(aux);
-    for (var j = 0; j < palavras; j++) {
-        if (j == aux - 1) {
-            array_palavras[j] = "PEDRO" //substitui a palavra por PEDRO
+            }
+            n_musica = n_musica.concat(n_linha, "\n");
         }
-        n_linha = n_linha.concat(" ", array_palavras[j]);
+        $('#posText').val(n_musica);
 
     }
-    n_musica = n_musica.concat(n_linha, "\n");
-}
-bot.sendMessage({
-    to: channelID,
-    message: String(n_musica)
-});
+})
